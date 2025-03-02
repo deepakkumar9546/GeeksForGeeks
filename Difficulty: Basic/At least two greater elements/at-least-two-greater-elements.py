add4e77e4c43fe76df3e1518326c46f8925f124a
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2025-03-03 00:40:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/At least two greater elements/at-least-two-greater-elements.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+    def findElements(self,arr):
+        arr.sort()
+        return arr[:len(arr)-2]
+            
+        # Your code goes here
+    
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+def main():
+
+    T = int(input())
+
+    while (T > 0):
+
+        arr = [int(x) for x in input().strip().split()]
+        ob = Solution()
+        print(*ob.findElements(arr))
+
+        T -= 1
+        print("~")
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
