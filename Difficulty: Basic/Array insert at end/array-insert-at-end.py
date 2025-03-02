b9a6f0bcf06c388ea1c5c33ed7b5c79997397fac
Deepak Kumar <137646137+deepakkumar9546@users.noreply.github.com>
2025-03-03 00:34:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array insert at end/array-insert-at-end.py
@@ -0,0 +1,46 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+
+# You only need to insert the given element at 
+# the end, i.e., at index sizeOfArray - 1. You may 
+# assume that the array already has sizeOfArray - 1
+# elements. 
+def insertAtEnd(arr,sizeOfArray,element):
+    s=arr.append(element)
+    return s
+    ##Your code here
+
+
+
+#{ 
+ # Driver Code Starts.
+
+
+def main():
+    testcases=int(input())
+    
+    while(testcases>0):
+        sizeOfArray=int(input())
+        
+        arr=[int(x) for x in input().strip().split()]
+        
+        element=int(input())
+        
+        insertAtEnd(arr,sizeOfArray,element)
+        
+        for i in arr:
+            print(i,end=" ")
+        print()
+        
+        testcases-=1
+        print("~")
+    
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
